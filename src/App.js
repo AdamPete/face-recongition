51ef5e0b86e348350d8a363ca9d203a90abc67af
AdamPete <adampetebuz@protonmail.com>
2022-01-21 21:15:14 -0600

    The frontend styling for project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Navigation from './components/nav/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <Logo />
-      {/* 
+      <Rank />
       <ImageLinkForm />
+      {/* 
       <FaceRecongnition />} */}
     </div>
   );
